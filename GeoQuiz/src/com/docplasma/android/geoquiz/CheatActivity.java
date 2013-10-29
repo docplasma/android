df7312ba67def3c70c76fac74a904dc6dc5aef5a
docplasma <thedocplasma@gmail.com>
2013-10-29 09:44:58 -0500

    Added Cheat

---
+++ GeoQuiz/src/com/docplasma/android/geoquiz/CheatActivity.java
@@ -0,0 +1,22 @@
+package com.docplasma.android.geoquiz;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CheatActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.cheat, menu);
+		return true;
+	}
+
+}
